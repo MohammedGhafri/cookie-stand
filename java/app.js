fc13fcd924e01325356e60bf19299986b44c3f4d
MohammedGhafri <eng.m.ghafri@gmail.com>
2020-05-17 23:06:29 +0300

    lab602

--- java/app.js
+++ java/app.js
@@ -5,8 +5,9 @@
 // Dubai	11	38	3.7
 // Paris	20	38	2.3
 // Lima	    2	16	4.6
-
-var seattle ={                 
+var hourArray=['6am', '7am', '8am' ,'9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+var seattle ={ 
+    name : 'seattle',              
     minCust: 23 ,
     maxCust: 65,
     avgCookieSale:6.3 ,
@@ -14,46 +15,128 @@ var seattle ={
     rarray: [],
     
     randomCust: function(){
-        // return Math.floor(Math.random() * Math.floor(max));
-        // var max= Math.floor(Math.random() * Math.floor(1000));
-        // console.log(max);
-        // console.log(max-10);
-        this.rcust=randomCustomers(Math.floor(Math.random() * Math.floor(1000)));
-        console.log(seattle.rcust);
+        
+        this.rcust=randomCustomers(Math.floor(Math.random() * Math.floor(100)));
+        // console.log(seattle.rcust);
         this.rarray=calculation(seattle.rcust);
     }
     
 }
-seattle.randomCust();
-console.log(seattle);
-console.log(seattle.rarray);
-console.log(seattle.rcust);
 
-// console.log(a);
+
+seattle.randomCust();
+
+
+
+
+
+
+var container = document.getElementById('seattle');
+var header2 = document.createElement('h2');
+header2.textContent=seattle.name;
+container.appendChild(header2);
+var unlisted = document.createElement('ul');
+container.appendChild(unlisted);
+createUnlested1();
 
 var tokyo ={
+    name: 'tokyo',
     minCust: 3,
     maxCust: 24,
     avgCookieSale: 1.2,
+    rcust:[],
+    rarray: [],
+
+    randomCust: function(){
+        
+        this.rcust=randomCustomers(Math.floor(Math.random() * Math.floor(100)));
+        // console.log(seattle.rcust);
+        this.rarray=calculation(tokyo.rcust);
+    }
 }
+tokyo.randomCust();
+
+var container = document.getElementById('seattle');
+var header2 = document.createElement('h2');
+header2.textContent=tokyo.name;
+container.appendChild(header2);
+var unlisted = document.createElement('ul');
+container.appendChild(unlisted);
+createUnlested2();
+
+
+
+
 
 var dubai ={
+    name:'dubai',
     minCust:11 ,
     maxCust: 38,
     avgCookieSale:3.7 ,
+    randomCust: function(){
+        
+        this.rcust=randomCustomers(Math.floor(Math.random() * Math.floor(100)));
+        // console.log(seattle.rcust);
+        this.rarray=calculation(dubai.rcust);
+    }
 }
+dubai.randomCust();
+var container = document.getElementById('seattle');
+var header2 = document.createElement('h2');
+header2.textContent=dubai.name;
+container.appendChild(header2);
+var unlisted = document.createElement('ul');
+container.appendChild(unlisted);
+createUnlested3();
 
 var paris= {
+    name:'paris',
     minCust:20 ,
     maxCust: 38,
     avgCookieSale: 2.3,
+    rcust:[],
+    rarray: [],
+    
+    randomCust: function(){
+        
+        this.rcust=randomCustomers(Math.floor(Math.random() * Math.floor(100)));
+        // console.log(seattle.rcust);
+        this.rarray=calculation(seattle.rcust);
+    }
 }
+paris.randomCust();
+var container = document.getElementById('seattle');
+var header2 = document.createElement('h2');
+header2.textContent=paris.name;
+container.appendChild(header2);
+var unlisted = document.createElement('ul');
+container.appendChild(unlisted);
+createUnlested4();
+
 
 var lima ={
+    name:'lima',
     minCust: 2,
     maxCust: 16,
     avgCookieSale:4.6 ,
+    rcust:[],
+    rarray: [],
+    
+    randomCust: function(){
+        
+        this.rcust=randomCustomers(Math.floor(Math.random() * Math.floor(100)));
+        // console.log(seattle.rcust);
+        this.rarray=calculation(lima.rcust);
+    }
 }
+lima.randomCust();
+var container = document.getElementById('seattle');
+var header2 = document.createElement('h2');
+header2.textContent=lima.name;
+container.appendChild(header2);
+var unlisted = document.createElement('ul');
+container.appendChild(unlisted);
+createUnlested5();
 
 // Seattle	23	65	6.3
 // Tokyo	3	24	1.2
@@ -66,28 +149,68 @@ function randomCustomers(max){// generate random customers for all objects
    var c=Math.floor(Math.random() * Math.floor(max));
    randomCust.push(c);}
    
-    return randomCust; //Math.floor(Math.random() * Math.floor(max));
+    return randomCust; 
 }
 
-// var arr = [];
-//         for (var i = 0; i < 5; i++)
-//         {
-//             arr.push(Math.floor(Math.random() * 6) + 1)
-//         }
-//         console.log(arr);
+
 
 function calculation(a){
-    // console.log(a);
+    
     var arrr=[];
     var b=[];
     for (var i=0; i<14; i++){
-// var b=Math.floor( seattle.avgCookieSale * a);
-// var b= Math.round(seattle.avgCookieSale) * (Math.floor(Math.random() * Math.floor(1000)));
+
  b[i]= Math.round(seattle.avgCookieSale) * a[i];
-// console.log(b);
+
 arrr.push(b[i]);
-// console.log(arrr);
+
     }
     return arrr;
-    // console.log(b);
+    
+}
+function createUnlested1 (){
+    
+    for(var i= 0 ; i<hourArray.length; i++){
+var list1=document.createElement('li');
+unlisted.appendChild(list1);
+list1.textContent=`${ hourArray[i]} : ${seattle.rarray[i]} cookies`;
+    }
+}
+
+
+
+function createUnlested2 (){
+    
+    for(var i= 0 ; i<hourArray.length; i++){
+var list1=document.createElement('li');
+unlisted.appendChild(list1);
+list1.textContent=`${ hourArray[i]} : ${tokyo.rarray[i]} cookies`;
+    }
+}
+
+function createUnlested3 (){
+    
+    for(var i= 0 ; i<hourArray.length; i++){
+var list1=document.createElement('li');
+unlisted.appendChild(list1);
+list1.textContent=`${ hourArray[i]} : ${dubai.rarray[i]} cookies`;
+    }
+}
+
+function createUnlested4 (){
+    
+    for(var i= 0 ; i<hourArray.length; i++){
+var list1=document.createElement('li');
+unlisted.appendChild(list1);
+list1.textContent=`${ hourArray[i]} : ${paris.rarray[i]} cookies`;
+    }
+}
+
+function createUnlested5 (){
+    
+    for(var i= 0 ; i<hourArray.length; i++){
+var list1=document.createElement('li');
+unlisted.appendChild(list1);
+list1.textContent=`${ hourArray[i]} : ${lima.rarray[i]} cookies`;
+    }
 }
